0ef20c4ef7fb067f1e52d08aa0a6f97e99521cfb
aexol <aexol@aexol.com>
2019-01-25 14:22:03 +0100

    justa added info TODO

--- src/Utils/index.ts
+++ src/Utils/index.ts
@@ -5,6 +5,7 @@ import { NodeDefinition } from "../Models/NodeDefinition";
 
 export { MinimapUtils } from "./minimapUtils";
 
+//TODO: Replace with uuid RFC compliant library
 function uuidv4() {
   return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
     var r = (Math.random() * 16) | 0,
