91a248f0e4e68e8ff87d23e6bb250b6a060323c1
Ania Łysiuk <anna.lysiuk86@gmail.com>
2019-11-18 10:03:22 +0100

    separate colors palette for light theme, object copy correction

---
+++ src/Theme/ColorsLight.ts
@@ -0,0 +1,22 @@
+export const Colors = {
+  grey: [
+    "#FBFBFB",
+    "#E6E6E6",
+    "#CCCCCC",
+    "#B2B2B2",
+    "#808080",
+    "#666666"
+  ],
+  main: [
+    "#FF59C5"
+  ],
+  green: [
+    "#30FFC8"
+  ],
+  yellow: [
+    "#C3A240"
+  ],
+  red: [
+    "#FF306B"
+  ],
+};
--- src/Theme/DefaultDiagramThemeLight.ts
+++ src/Theme/DefaultDiagramThemeLight.ts
@@ -1,23 +1,23 @@
 import { DiagramTheme } from "../Models";
 import { DefaultDiagramTheme } from "./DefaultDiagramTheme";
-import { Colors } from "./Colors";
+import { Colors } from "./ColorsLight";
 
 export const DefaultDiagramThemeLight: DiagramTheme = {
-  ...JSON.parse(JSON.stringify(DefaultDiagramTheme)),
+  ...DefaultDiagramTheme,
   colors: {
-    background: Colors.grey[10],
+    background: Colors.grey[1],
     node: {
       background: Colors.grey[2],
       selected: Colors.green[0],
-      name: Colors.grey[11],
-      type: Colors.grey[6],
+      name: Colors.grey[5],
+      type: Colors.grey[5],
       types: {
         string: Colors.green[0],
-        type: Colors.grey[11]
+        type: Colors.grey[5]
       },
       options: {
         required: Colors.red[0],
-        array: Colors.yellow[7]
+        array: Colors.yellow[0]
       },
       hover: {
         type: Colors.yellow[0]
@@ -26,12 +26,12 @@ export const DefaultDiagramThemeLight: DiagramTheme = {
     minimap: {
       background: Colors.grey[0],
       visibleArea: Colors.grey[1],
-      node: Colors.grey[11],
+      node: Colors.grey[5],
       borders: Colors.grey[4]
     },
     description: {
       background: Colors.grey[2],
-      text: Colors.grey[11]
+      text: Colors.grey[5]
     },
     port: {
       background: Colors.grey[3],
@@ -46,12 +46,12 @@ export const DefaultDiagramThemeLight: DiagramTheme = {
     menu: {
       background: Colors.grey[2],
       borders: Colors.grey[3],
-      text: Colors.grey[11],
+      text: Colors.grey[5],
       hover: Colors.grey[1]
     },
     help: {
       background: Colors.grey[2],
-      text: Colors.grey[11],
+      text: Colors.grey[5],
       title: Colors.main[0]
     }
   }
